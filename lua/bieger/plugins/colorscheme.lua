d04a6cea77f449ae84f79df0a81eae076b0fffb4
Eduardo <eduardo42bieger@gmail.com>
2024-05-24 14:32:42 -0300

    Update theme

--- lua/bieger/plugins/colorscheme.lua
+++ lua/bieger/plugins/colorscheme.lua
@@ -1,9 +1,18 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "olimorris/onedarkpro.nvim",
   priority = 1000,
 
   config = function()
-    vim.cmd([[colorscheme catppuccin-macchiato]])
+    vim.cmd([[colorscheme onedark]])
   end
 }
+
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+
+--   config = function()
+--     vim.cmd([[colorscheme catppuccin-macchiato]])
+--   end
+-- }
