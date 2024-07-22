796beff80845b5d4f56c8f65ecd28a1f38443b0b
Eduardo <eduardo42bieger@gmail.com>
2024-07-22 15:05:38 -0300

    Install trouble plugin

---
+++ lua/bieger/plugins/trouble.lua
@@ -0,0 +1,44 @@
+return {
+  "folke/trouble.nvim",
+  config = function()
+    require("trouble").setup({
+      auto_preview = false,
+      auto_fold = true,
+      use_lsp_diagnostic_signs = true,
+    })
+  end,
+
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+}
