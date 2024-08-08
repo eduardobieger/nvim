86b9816ff4abf335c4f19415c2f89dc39114a31d
Eduardo <eduardo42bieger@gmail.com>
2024-08-08 08:46:01 -0300

    Istall Cyberdream color scheme

--- lua/bieger/plugins/colorscheme.lua
+++ lua/bieger/plugins/colorscheme.lua
@@ -1,18 +1,44 @@
 return {
-	"drewtempelmeyer/palenight.vim",
+	"scottmckendry/cyberdream.nvim",
 	lazy = false,
 	priority = 1000,
 
 	config = function()
-		vim.cmd([[colorscheme palenight]])
+		require("cyberdream").setup({
+			transparent = true,
+			italic_comments = true,
+			hide_fillchars = false,
+			borderless_telescope = false,
+			terminal_colors = true,
+			cache = false,
+			theme = {
+				variant = "default",
+			},
+			extensions = {
+				telescope = true,
+				notify = true,
+			},
+		})
+
+		vim.cmd([[colorscheme cyberdream]])
 	end,
 }
+
+-- return {
+-- 	"drewtempelmeyer/palenight.vim",
+-- 	lazy = false,
+-- 	priority = 1000,
+--
+-- 	config = function()
+-- 		vim.cmd([[colorscheme palenight]])
+-- 	end,
+-- }
 
 -- return {
 --   "rebelot/kanagawa.nvim",
 --   lazy = false,
 --   priority = 1000,
--- 
+--
 --   config = function()
 --     vim.cmd([[colorscheme kanagawa-wave]])
 --   end
@@ -32,7 +58,7 @@ return {
 -- return {
 --   "olimorris/onedarkpro.nvim",
 --   priority = 1000,
--- 
+--
 --   config = function()
 --     vim.cmd([[colorscheme onedark]])
 --   end
