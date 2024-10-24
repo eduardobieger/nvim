f9816894b066a5c3a581a109d3ccd029a46a12b2
Eduardo <eduardo42bieger@gmail.com>
2024-10-24 16:21:39 -0300

    Install volar lsp

--- lua/bieger/plugins/lsp/lspconfig.lua
+++ lua/bieger/plugins/lsp/lspconfig.lua
@@ -125,6 +125,11 @@ return {
 			},
 		})
 
+		lspconfig["volar"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
 		-- configure lua server (with special settings)
 		lspconfig["lua_ls"].setup({
 			capabilities = capabilities,
