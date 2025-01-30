f5f8d3c03060fc45dafcf9ebe4db1c92dedee2d6
Eduardo <eduardo42bieger@gmail.com>
2025-01-30 09:55:51 -0300

    Install plugin for Java and configure new colorscheme

--- lua/bieger/plugins/colorscheme.lua
+++ lua/bieger/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
-return {
-	"sainnhe/gruvbox-material",
-	lazy = false,
-	priority = 1000,
-
-	config = function()
-		vim.g.gruvbox_material_foreground = "original"
-		vim.g.gruvbox_material_background = "soft"
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme([[gruvbox-material]])
-	end,
-}
+-- return {
+-- 	"sainnhe/gruvbox-material",
+-- 	lazy = false,
+-- 	priority = 1000,
+--
+-- 	config = function()
+-- 		vim.g.gruvbox_material_foreground = "original"
+-- 		vim.g.gruvbox_material_background = "soft"
+-- 		vim.g.gruvbox_material_enable_italic = true
+-- 		vim.cmd.colorscheme([[gruvbox-material]])
+-- 	end,
+-- }
 
 -- return {
 -- 	"uloco/bluloco.nvim",
@@ -54,14 +54,14 @@ return {
 -- 	end,
 -- }
 
--- return {
---   "olimorris/onedarkpro.nvim",
---   priority = 1000,
---
---   config = function()
---     vim.cmd([[colorscheme onedark]])
---   end
--- }
+return {
+	"olimorris/onedarkpro.nvim",
+	priority = 1000,
+
+	config = function()
+		vim.cmd([[colorscheme onedark]])
+	end,
+}
 
 -- return {
 --   "catppuccin/nvim",
