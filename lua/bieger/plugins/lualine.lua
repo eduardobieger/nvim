f84107d0def8ed4d0021f5a98641f336befdec75
Eduardo <eduardo42bieger@gmail.com>
2025-02-04 08:00:41 -0300

    Remove copilot-lualine plugin

--- lua/bieger/plugins/lualine.lua
+++ lua/bieger/plugins/lualine.lua
@@ -25,31 +25,6 @@ return {
 				},
 				lualine_c = { "filename" },
 				lualine_x = {
-					{
-						"copilot",
-						symbols = {
-							status = {
-								icons = {
-									enabled = " ",
-									sleep = " ",
-									disabled = " ",
-									warning = " ",
-									unknown = " ",
-								},
-								hl = {
-									enabled = "#50FA7B",
-									sleep = "#AEB7D0",
-									disabled = "#6272A4",
-									warning = "#FFB86C",
-									unknown = "#FF5555",
-								},
-							},
-							spinners = require("copilot-lualine.spinners").dots,
-							spinner_color = "#6272A4",
-						},
-						show_colors = false,
-						show_loading = true,
-					},
 					"encoding",
 					"fileformat",
 					"filetype",
