bdab815db519fa958935e8ac4da3466028e46060
Eduardo Bieger <eduardo42bieger@gmail.com>
2025-05-20 13:18:59 -0300

    feat: add new color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,12 +12,43 @@
 -- 	end,
 -- }
 
+-- return {
+--   "rose-pine/neovim",
+--   lazy = false,
+--   priority = 1000,
+--   name = "rose-pine",
+--   config = function()
+--     vim.cmd "colorscheme rose-pine"
+--   end,
+-- }
+
+-- return {
+--   "sainnhe/sonokai",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.g.sonokai_enable_italic = true
+--     vim.g.sonokai_style = "atlantis"
+--     vim.cmd.colorscheme "sonokai"
+--   end,
+-- }
+
+-- return {
+--   "sainnhe/everforest",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.g.everforest_enable_italic = true
+--     vim.g.everforest_background = "hard"
+--     vim.cmd.colorscheme "everforest"
+--   end,
+-- }
+
 return {
-  "rose-pine/neovim",
+  "rebelot/kanagawa.nvim",
   lazy = false,
   priority = 1000,
-  name = "rose-pine",
   config = function()
-    vim.cmd "colorscheme rose-pine"
+    vim.cmd.colorscheme "kanagawa-wave"
   end,
 }
