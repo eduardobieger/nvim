2c3c17f9ce24222aead5af0ad20801b88d7e47ec
Eduardo Bieger <eduardo42bieger@gmail.com>
2025-09-22 13:35:09 -0300

    feat: remove unused color schemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,44 +1,8 @@
--- return {
--- 	"catppuccin/nvim",
--- 	lazy = false,
--- 	name = "catppuccin",
--- 	priority = 1000,
--- 	config = function()
---         require("catppuccin").setup({
---             transparent_background = false,
---         })
---
---         vim.cmd.colorscheme "catppuccin-mocha"
--- 	end,
--- }
-
--- return {
---   "rose-pine/neovim",
---   lazy = false,
---   priority = 1000,
---   name = "rose-pine",
---   config = function()
---     vim.cmd "colorscheme rose-pine"
---   end,
--- }
-
--- return {
---   "rebelot/kanagawa.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd.colorscheme "kanagawa-wave"
---   end,
--- }
-
 return {
-  "sainnhe/gruvbox-material",
+  "joshdick/onedark.vim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_enable_bold = true
-    vim.g.gruvbox_material_background = "hard"
-    vim.cmd.colorscheme "gruvbox-material"
+    vim.cmd.colorscheme "onedark"
   end,
 }
