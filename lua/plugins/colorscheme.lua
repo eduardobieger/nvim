95086a5826beff5d831b9fc7743772cceb06107d
Eduardo Bieger <eduardo42bieger@gmail.com>
2025-11-02 20:35:49 -0300

    feat: set ayu-mirage as colorscheme with transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
-  "scottmckendry/cyberdream.nvim",
+  "Shatur/neovim-ayu",
   lazy = false,
   priority = 1000,
   config = function()
-    require("cyberdream").setup {
-      transparent = true,
-      italic_comments = true,
+    require("ayu").setup {
+      mirage = true,
+      overrides = {
+        Normal = { bg = "None" },
+        NormalFloat = { bg = "none" },
+        ColorColumn = { bg = "None" },
+        SignColumn = { bg = "None" },
+        Folded = { bg = "None" },
+        FoldColumn = { bg = "None" },
+        CursorLine = { bg = "None" },
+        CursorColumn = { bg = "None" },
+        VertSplit = { bg = "None" },
+      },
     }
-    vim.cmd.colorscheme "cyberdream"
+    vim.cmd.colorscheme "ayu"
   end,
 }
