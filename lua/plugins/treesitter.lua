ccc43365312d0b82dce467c41397af667c8465d2
Eduardo Bieger <eduardo42bieger@gmail.com>
2026-02-02 14:59:07 -0300

    fix: syntax highlight not working in Go files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return { -- Highlight, edit, and navigate code
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  -- main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
@@ -20,6 +20,9 @@ return { -- Highlight, edit, and navigate code
       "toml",
       "sql",
       "go",
+      "gomod",
+      "gosum",
+      "gowork",
       "javascript",
       "typescript",
       "python",
@@ -37,9 +40,9 @@ return { -- Highlight, edit, and navigate code
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
+      additional_vim_regex_highlighting = false,
     },
-    indent = { enable = true, disable = { "ruby" } },
+    event = { "BufReadPost", "BufNewFile" },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
